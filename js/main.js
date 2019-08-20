315574f9cbe711b40353adc8ad6868258a84e4c1
Denis Kisina <kisinad@gmail.com>
2019-08-20 20:05:34 +0300

    worked on the previous button

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ window.onload = () => {
     });
     $("#next-page").on("click", function(){
         let currentPage = $(".pagination li.active").index();
-        if (currentPage==totalPages){
+        if (currentPage===totalPages){
             return false;
         } else {
             currentPage++;
@@ -41,7 +41,22 @@ window.onload = () => {
             }
             $(".pagination li.current-page:eq("+(currentPage-1)+")").addClass("active");
         }
-    })
+    });
+    $("#previous-page").on("click", function(){
+        let currentPage = $(".pagination li.active").index();
+        if (currentPage===1){
+            return false;
+        } else {
+            currentPage--;
+            $(".pagination li").removeClass("active");
+            $(".game-body .items").hide();
+            let grandTotal = limitPerPage*currentPage;
+            for (let i=grandTotal-limitPerPage; i<grandTotal; i++){
+                $(".game-body .items:eq("+i+")").show();
+            }
+            $(".pagination li.current-page:eq("+(currentPage-1)+")").addClass("active");
+        }
+    });
 }
 
 

dead6d4c98741d19d1734c54ba972c42da5d17c1
Denis Kisina <kisinad@gmail.com>
2019-08-24 02:04:59 +0300

    added sort by ascending

--- js/main.js
+++ js/main.js
@@ -1,16 +1,140 @@
 window.onload = () => { //Waits for the whole document to load then runs the scripts inside
     loadGameData(gameData); //Function to auto create and inject divs inside DOM
     searchGameName();
-    // ------------------------------------------------------
-    //JQuery code to generate pagination 
+   // sortAscending(gameData);
+    // sortDescending (gameData)
+    pagination(); 
+
+    $('.sortAsced').on('click', function(){
+        sortAscending(gameData);
+    });
+    
+}
+// Dummy sample data
+let gameData = [
+    {title: 'Street Fighter', score: 6.0,genre:'Action', platform:'PC game', editors_choice:'', src:'./img' },
+    {title: 'FIFA 18', score: 7.9,genre:'Role-playing', platform:'Microsoft-Xbox', editors_choice:'' },
+    {title: 'Super Mario Bros', score: 9.0,genre:'Action-adventure', platform:'Xbox-360', editors_choice:'' },
+    {title: 'League of Legends', score: 9.0,genre:'Role-playing', platform:'PlayStation 2', editors_choice:'' },
+    {title: 'Gigantic', score: 9.0,genre:'Action', platform:'Nitendo', editors_choice:'' },
+    {title: 'Mortal Kombat', score: 9.0,genre:'Action', platform:'Game Boy', editors_choice:'' },
+    {title: 'Super Smash Bros', score: 9.0,genre:'Role-playing', platform:'Xbox-360', editors_choice:'' },
+    {title: 'Apex Legends', score: 9.0,genre:'Action', platform:'Xbox-360', editors_choice:'' },
+    {title: 'Resident Evil', score: 9.0,genre:'Action', platform:'playStation 2', editors_choice:'' },
+    {title: 'Resident Evil', score: 3.0,genre:'Action', platform:'playStation 2', editors_choice:'' },
+    {title: 'Resident Evil', score: 9.0,genre:'Action', platform:'playStation 2', editors_choice:'' },
+    {title: 'Resident Evil', score: 4.0,genre:'Action', platform:'playStation 2', editors_choice:'' },
+    {title: 'Resident Evil', score: 9.0,genre:'Action', platform:'playStation 2', editors_choice:'' },
+    {title: 'Resident Evil', score: 9.0,genre:'Action', platform:'playStation 2', editors_choice:'' },
+    {title: 'Resident Evil', score: 2.0,genre:'Action', platform:'playStation 2', editors_choice:'' },
+    {title: 'Street Fighter', score: 9.0,genre:'Action', platform:'PC game', editors_choice:'', src:'./img' },
+    {title: 'FIFA 18', score: 7.9,genre:'Role-playing', platform:'Microsoft-Xbox', editors_choice:'' },
+    {title: 'Super Mario Bros', score: 9.0,genre:'Action-adventure', platform:'Xbox-360', editors_choice:'' },
+    {title: 'League of Legends', score: 9.0,genre:'Role-playing', platform:'PlayStation 2', editors_choice:'' },
+    {title: 'Gigantic', score: 9.0,genre:'Action', platform:'Nitendo', editors_choice:'' },
+    {title: 'Street Fighter', score: 9.0,genre:'Action', platform:'PC game', editors_choice:'', src:'./img' },
+    {title: 'FIFA 18', score: 7.9,genre:'Role-playing', platform:'Microsoft-Xbox', editors_choice:'' },
+    {title: 'Super Mario Bros', score: 9.0,genre:'Action-adventure', platform:'Xbox-360', editors_choice:'' },
+    {title: 'League of Legends', score: 9.0,genre:'Role-playing', platform:'PlayStation 2', editors_choice:'' },
+    {title: 'Gigantic', score: 9.0,genre:'Action', platform:'Nitendo', editors_choice:'' }
+    
+];
+let searchResults =[];
+//------------------------------------------------------------------------------
+//Function to create div cards
+//------------------------------------------------------------------------------
+function loadGameData (gameData) {
+    for (let game of gameData){
+        // console.log(game);
+    let newDiv = document.createElement('div');
+    newDiv.className = "items";
+    let newDivImg = document.createElement('div');
+    newDivImg.className = "img-item";
+    let newH3 = document.createElement('h3');
+    newH3.className = "title";
+    // newH3.setAttribute("data", `${game.title}`);
+    let newP = document.createElement('p');
+    newP.className = "platform";
+    
+    let newImg = document.createElement('img'); // Creates img
+    let newImgStar = document.createElement('img');
+    newImg.setAttribute("src", "./img/image-placeholder1.png");
+    let newSpanImg = document.createElement('span');
+    newSpanImg.className = "star";
+    newImgStar.setAttribute("src", "./img/star 25x25.png");
+    let newSpanGenre = document.createElement('span');
+    newSpanGenre.className = "genre";
+    let newSpanName = document.createElement('span');
+    let newSpanScore = document.createElement('span');
+    newSpanScore.className = "score";
+    newSpanScore.setAttribute("data-score", `${game.score}`);
+    let place = document.getElementsByClassName('main')[0];
+    let item = place.getElementsByClassName('game-body')[0];
+    let title = document.createTextNode(`${game.title}`);
+    title.className = "title";
+    item.appendChild(newDiv);
+    newDiv.appendChild(newDivImg);
+    newDiv.appendChild(newH3);
+    newH3.append(game.title);
+    newDiv.appendChild(newP);
+    newDiv.appendChild(newSpanImg);
+    newP.append(game.platform);
+    newSpanImg.appendChild(newImgStar);
+    newSpanGenre.appendChild(newSpanName);
+    newSpanName.append('Genre:');
+    newDiv.appendChild(newSpanGenre);
+    newSpanGenre.append(game.genre);
+    newDivImg.appendChild(newImg);
+    newDiv.appendChild(newSpanScore);
+    newSpanScore.append(game.score);
+ }
+}
+//-------------------------------------------------------------------------------------------------
+//Searching Function
+//----------------------------------------------------------------------------------------------
+ function searchGameName(){
+     let input =document.getElementById("search");
+     let filter = input.value.toUpperCase();
+     let div2 = document.getElementsByClassName("items");
+     let h3 = document.getElementsByClassName("title");
+     for(let i=0; i<div2.length-1; i++){
+        
+        h3 = div2[i].getElementsByClassName("title")[0];
+        if (h3.innerHTML.toUpperCase().indexOf(filter)>-1){
+            div2[i].style.display="";
+            }
+            else{
+            div2[i].style.display="none";
+        }
+     }
+     pagtest();
+}
+function searchGameNameTest(gameData){
+    let input =document.getElementById("search");
+    let filter = input.value.toUpperCase();
+    let div2 = document.getElementsByClassName("items");
+    let h3 = document.getElementsByClassName("title");
+    for(let i=0; i<gameData.length-1; i++){
+       h3 = gameData[i].title;
+       if (h3.innerHTML.toUpperCase().indexOf(filter)>-1){
+        gameData[i].style.display="";
+           }
+           else{
+           gameData[i].style.display="none";
+       }
+    }
+    pagtest();
+}
+// ------------------------------------------------------
+    //JQuery code to generate pagination function
     //-------------------------------------------------------
+    
+ function pagination(){
     let numberOfVariables = $(".game-body .items").length;
     let limitPerPage = 10;
     $(".game-body .items:gt("+(limitPerPage-1)+")").hide();
     let totalPages = Math.round(numberOfVariables/limitPerPage);
     $(".pagination").append("<li class='page-item current-page active'><a class='page-link' href='javascript:void(0)'>"+1+"</a></li>");
-   
-    
     for (let i =2; i<=totalPages; i++){
         $(".pagination").append("<li class='current-page' class='page-item'><a class='page-link' href='javascript:void(0)'>"+i+"</a></li>")
     }
@@ -62,110 +186,38 @@ window.onload = () => { //Waits for the whole document to load then runs the scr
             $(".pagination li.current-page:eq("+(currentPage-1)+")").addClass("active");
         }
     });
+ }
+
+function pagtest(){
+    let numberOfVariables = $(".game-body .items").length;
+    let limitPerPage = 10;
+    $(".game-body .items:gt("+(limitPerPage-1)+")").hide();
+    let totalPages = Math.round(numberOfVariables/limitPerPage);
+    
 }
-//---------------------------------------------------------------------
-//End of the pagination JQuery Code
-//------------------------------------------------------------------------
-
-
-// Dummy sample data
-let gameData = [
-    {title: 'Street Fighter', score: 9.0,genre:'Action', platform:'PC game', editors_choice:'', src:'./img' },
-    {title: 'FIFA 18', score: 7.9,genre:'Role-playing', platform:'Microsoft-Xbox', editors_choice:'' },
-    {title: 'Super Mario Bros', score: 9.0,genre:'Action-adventure', platform:'Xbox-360', editors_choice:'' },
-    {title: 'League of Legends', score: 9.0,genre:'Role-playing', platform:'PlayStation 2', editors_choice:'' },
-    {title: 'Gigantic', score: 9.0,genre:'Action', platform:'Nitendo', editors_choice:'' },
-    {title: 'Mortal Kombat', score: 9.0,genre:'Action', platform:'Game Boy', editors_choice:'' },
-    {title: 'Super Smash Bros', score: 9.0,genre:'Role-playing', platform:'Xbox-360', editors_choice:'' },
-    {title: 'Apex Legends', score: 9.0,genre:'Action', platform:'Xbox-360', editors_choice:'' },
-    {title: 'Resident Evil', score: 9.0,genre:'Action', platform:'playStation 2', editors_choice:'' },
-    {title: 'Resident Evil', score: 9.0,genre:'Action', platform:'playStation 2', editors_choice:'' },
-    {title: 'Resident Evil', score: 9.0,genre:'Action', platform:'playStation 2', editors_choice:'' },
-    {title: 'Resident Evil', score: 9.0,genre:'Action', platform:'playStation 2', editors_choice:'' },
-    {title: 'Resident Evil', score: 9.0,genre:'Action', platform:'playStation 2', editors_choice:'' },
-    {title: 'Resident Evil', score: 9.0,genre:'Action', platform:'playStation 2', editors_choice:'' },
-    {title: 'Resident Evil', score: 9.0,genre:'Action', platform:'playStation 2', editors_choice:'' },
-    {title: 'Street Fighter', score: 9.0,genre:'Action', platform:'PC game', editors_choice:'', src:'./img' },
-    {title: 'FIFA 18', score: 7.9,genre:'Role-playing', platform:'Microsoft-Xbox', editors_choice:'' },
-    {title: 'Super Mario Bros', score: 9.0,genre:'Action-adventure', platform:'Xbox-360', editors_choice:'' },
-    {title: 'League of Legends', score: 9.0,genre:'Role-playing', platform:'PlayStation 2', editors_choice:'' },
-    {title: 'Gigantic', score: 9.0,genre:'Action', platform:'Nitendo', editors_choice:'' },
-    {title: 'Street Fighter', score: 9.0,genre:'Action', platform:'PC game', editors_choice:'', src:'./img' },
-    {title: 'FIFA 18', score: 7.9,genre:'Role-playing', platform:'Microsoft-Xbox', editors_choice:'' },
-    {title: 'Super Mario Bros', score: 9.0,genre:'Action-adventure', platform:'Xbox-360', editors_choice:'' },
-    {title: 'League of Legends', score: 9.0,genre:'Role-playing', platform:'PlayStation 2', editors_choice:'' },
-    {title: 'Gigantic', score: 9.0,genre:'Action', platform:'Nitendo', editors_choice:'' }
+let resulta;
+let resultb;
+function sortAscending(gameData){
     
-];
-//------------------------------------------------------------------------------
-//Function to create div cards
-//------------------------------------------------------------------------------
-function loadGameData (gameData) {
-    for (let game of gameData){
-    let newDiv = document.createElement('div');
-    newDiv.className = "items";
-    let newDivImg = document.createElement('div');
-    newDivImg.className = "img-item";
-    let newH3 = document.createElement('h3');
-    newH3.className = "title";
-    let newP = document.createElement('p');
-    newP.className = "platform";
+    resulta = gameData.sort(function(a, b){
+        return a.score - b.score;
+    });
+    let sortResults="";
+    resulta.forEach(function(element){
+        sortResults +="<div class='items'>"+
+        "<div class='img-item'><img src='./img/image-placeholder1.png'></div>"+
+        "<h3 class='title'>"+element.title+"</h3><p class='platform'>"+element.platform+"</p>"+
+        "<span class='star'><img src='./img/star 25x25.png'></span>"+
+        "<span class='genre'><span>Genre:</span>"+element.genre+"</span><span class='score'>"+element.score+"</span></div>"
+    // console.log(resulta);
+});
+
+$('.game-body').html("");
+$('.game-body').html(sortResults);
+
     
-    let newImg = document.createElement('img'); // Creates img
-    let newImgStar = document.createElement('img');
-    newImg.setAttribute("src", "./img/image-placeholder1.png");
-    let newSpanImg = document.createElement('span');
-    newSpanImg.className = "star";
-    newImgStar.setAttribute("src", "./img/star 25x25.png");
-    let newSpanGenre = document.createElement('span');
-    newSpanGenre.className = "genre";
-    let newSpanName = document.createElement('span');
-    let newSpanScore = document.createElement('span');
-    newSpanScore.className = "score";
-    let place = document.getElementsByClassName('main')[0];
-    let item = place.getElementsByClassName('game-body')[0];
+ 
+    
+}
+
    
-    let title = document.createTextNode(`${game.title}`);
-        title.className = "title";
-    item.appendChild(newDiv);
-    newDiv.appendChild(newDivImg);
-    newDiv.appendChild(newH3);
-    newH3.append(game.title);
-    newDiv.appendChild(newP);
-    newDiv.appendChild(newSpanImg);
-    newP.append(game.platform);
-    newSpanImg.appendChild(newImgStar);
-    newSpanGenre.appendChild(newSpanName);
-    newSpanName.append('Genre:');
-    newDiv.appendChild(newSpanGenre);
-    newSpanGenre.append(game.genre);
-    
-    newDivImg.appendChild(newImg);
-    newDiv.appendChild(newSpanScore);
-    newSpanScore.append(game.score);
- }
-}
-
-//-------------------------------------------------------------------------------------------------
-//Searching Function
-//----------------------------------------------------------------------------------------------
- function searchGameName(){
-     let input =document.getElementById("search");
-     let filter = input.value.toUpperCase();
-     let div1 = document.getElementsByClassName("game-body");
-     let div2 = document.getElementsByClassName("items");
-     console.log(div2.length);
-     let h3 = document.getElementsByClassName("title");
-     console.log(h3);
-     for(let i=0; i<div2.length; i++){
-        let h3 = div2[i].getElementsByClassName("title")[0];
-        if (h3.innerHTML.toUpperCase().indexOf(filter)>-1){
-            div2[i].style.display="";
-
-        }else{
-            div2[i].style.display="none";
-        }
-     }
-     
-     
- }
